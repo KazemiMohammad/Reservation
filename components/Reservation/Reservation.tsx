607ff3ce343d1dce0fb4131e1d83ad639d764ef1
KazemiMohammad <Kazemi_mcp@hotmail.com>
2022-11-13 18:26:19 +0330

    feat (Reservation): Check working day

--- components/Reservation/Reservation.tsx
+++ components/Reservation/Reservation.tsx
@@ -1,3 +1,4 @@
+import { daysInWeek } from "date-fns";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
@@ -6,17 +7,25 @@ import ReservationList from "../ReservationList";
 
 const Reservation = () => {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [isWorkday, setIsWorkday] = useState<boolean>();
+
   const handleSelectDate = (value: Date) => {
+    setIsWorkday(checkIsWorkday(value));
     setSelectedDate(value);
   };
+
+  const checkIsWorkday = (value: Date) => ![6, 0].includes(value.getDay());
 
   return (
     <>
       <div>
-        <h1 className="text-lg font-bold text-violet-600">Select your date to reserve</h1>
+        <h1 className="text-lg font-bold text-violet-600">
+          Select your date to reserve
+        </h1>
         <Calendar onChange={handleSelectDate} minDate={new Date()} />
       </div>
-      <AddReservation />
+      {isWorkday&&<AddReservation selectedDate={selectedDate} />}
+      {!isWorkday&&<div className="text-violet-700 font-bold text-lg">Selected day is not a work day!</div>}
       <ReservationList reservationDate={selectedDate} />
     </>
   );
