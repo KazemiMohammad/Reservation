6df41df3ff51b6db1d1f8628eb6d89853fb23a8b
KazemiMohammad <Kazemi_mcp@hotmail.com>
2022-11-13 18:27:08 +0330

    feat (ReservationList): Refine texts and styles

--- components/ReservationList/ReservationList.tsx
+++ components/ReservationList/ReservationList.tsx
@@ -1,31 +1,49 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useReservation, Reservation } from "../../lib/useReservation";
-
+import { format, isSameDay } from "date-fns";
 interface ReservationListProps {
   reservationDate: Date;
 }
 
 const ReservationList = ({ reservationDate }: ReservationListProps) => {
   const { reservations } = useReservation();
-  return (
-    <div className="flex flex-col">
-      {reservations.map((reserve: Reservation, index: number) => {
-        return (
-          <div key={reserve.user + index} className="flex">
-            <div>
-              Name:<span>{reserve.user}</span>
-            </div>
-            <div>
-              Start:<span>{reserve.startDate}</span>
-            </div>
-            <div>
-              End:<span>{reserve.endDate}</span>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+
+  const filteredReservations = useMemo(
+    () =>
+      reservations.filter((reserve: Reservation) =>
+        [
+          format(reserve.startDate, "yyyyMMdd"),
+          format(reserve.endDate, "yyyMMdd"),
+        ].includes(format(reservationDate, "yyyMMdd"))
+      ),
+    [reservations, reservationDate]
   );
+  
+  const generateReservationString = (reserve: Reservation): string => {
+    return `${reserve.user} - ${formatDateToDisplay(
+      reserve.startDate
+    )} → ${formatDateToDisplay(reserve.endDate)}`;
+  };
+
+  const formatDateToDisplay = (value: Date) => {
+    let formatString: string = isSameDay(value, reservationDate)
+      ? "hh a"
+      : "EEEE hh a";
+    return format(value, formatString);  
+  };  
+
+  function generateListItems(): React.ReactNode {
+    return filteredReservations.map((reserve: Reservation, index: number) => (
+      <div
+        key={reserve.user + index}
+        className="flex rounded m-1 p-2 cursor-pointer bg-violet-200 hover:bg-violet-300 border border-violet-400"
+      >
+        {generateReservationString(reserve)}
+      </div>
+    ));
+  }
+
+  return <div className="flex flex-col">{generateListItems()}</div>;
 };
 
 export default ReservationList;
