cbf0a754468ac01536be28aad56d9baced2a6400
KazemiMohammad <Kazemi_mcp@hotmail.com>
2022-11-14 02:47:23 +0330

    feat (AddReservation): Implement overlap validation

--- components/AddReservation/AddReservation.tsx
+++ components/AddReservation/AddReservation.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Reservation, useReservation } from "../../lib/useReservation";
 import { useForm } from "react-hook-form";
-import { compareAsc } from "date-fns";
+import {
+  getOverlappingDaysInIntervals,
+  areIntervalsOverlapping,
+  format,
+  compareAsc,
+} from "date-fns";
+import useReservationValidator from "../../hooks/useReservationValidator";
 type AddReservationFormFields = {
   user: string;
   startHour: number;
@@ -10,7 +16,13 @@ type AddReservationFormFields = {
 };
 
 const AddReservation = ({ selectedDate }: { selectedDate: Date }) => {
-  const { addReservation } = useReservation();
+  const { reservations, addReservation } = useReservation();
+  const {
+    isUserUniqueInDay,
+    isEndHourValid,
+    isEndHourInValidOnNextDay,
+    hasSelectedRangeOverlap,
+  } = useReservationValidator(reservations);
 
   const {
     register,
@@ -29,30 +41,53 @@ const AddReservation = ({ selectedDate }: { selectedDate: Date }) => {
       </option>
     ));
   };
-  const validateTimes = (formData: AddReservationFormFields): boolean => {
-    const { startHour, endHour, endNextDay } = formData;
-    if (!endNextDay && endHour - startHour < 1) {
+  const isFormDataValid = (formData: AddReservationFormFields): boolean => {
+    const { user, startHour, endHour, endNextDay } = formData;
+
+    if (!isUserUniqueInDay(user, selectedDate)) {
+      setError("user", {
+        type: "value",
+        message: "Each user is allow to reserve once a day!",
+      });
+      return false;
+    }
+
+    if (!isEndHourValid(endNextDay, endHour, startHour)) {
       setError("endHour", {
         type: "pattern",
         message: "End time should be at least 1 hour after the Start time!",
       });
       return false;
     }
-    if (endNextDay && endHour>11) {
+
+    if (endNextDay && !isEndHourInValidOnNextDay(endHour)) {
       setError("endHour", {
         type: "pattern",
         message: "You could just reserve the car until 11 AM of next day!",
       });
       return false;
     }
-    
+
+    let startDate: Date = new Date(selectedDate);
+    startDate.setHours(formData.startHour);
+
+    let endDate: Date = new Date(selectedDate);
+    endDate.setHours(formData.endHour);
+    formData.endNextDay && endDate.setDate(endDate.getDate() + 1);
+
+    if (hasSelectedRangeOverlap(startDate, endDate)) {
+      setError("endHour", {
+        type: "pattern",
+        message: "There is a overlap with your selected times!",
+      });
+      return false;
+    }
+
     return true;
   };
 
   const onSubmit = handleSubmit((formData: AddReservationFormFields) => {
-    if (!validateTimes(formData)) {
-      return;
-    }
+    if (!isFormDataValid(formData)) return;
 
     let startDate: Date = new Date(selectedDate);
     startDate.setHours(formData.startHour);
@@ -75,25 +110,41 @@ const AddReservation = ({ selectedDate }: { selectedDate: Date }) => {
     <div className="rounded border border-violet-400 m-2 p-2 bg-white">
       <form onSubmit={onSubmit}>
         <div className="flex">
-          <input
-            {...register("user", { required: true })}
-            className="border rounded m-1 p-1"
-          />
-          <select {...register("startHour")} className="border rounded m-1 p-1">
-            {getAvailableTimeOptions(selectedDate)}
-          </select>
-          <select {...register("endHour")} className="border rounded m-1 p-1">
-            {getAvailableTimeOptions(selectedDate)}
-          </select>
-          <div className="m-1 p-1">
+          <div className="flex flex-col mr-1">
+            <label htmlFor="user" className="text-sm text-violet-600">
+              Name
+            </label>
+            <input
+              {...register("user", { required: true })}
+              className="border border-violet-200 rounded p-1"
+            />
+          </div>
+          <div className="flex flex-col mx-1">
+            <label htmlFor="user" className="text-sm text-violet-600">
+              From
+            </label>
+            <select {...register("startHour")} className="border rounded p-1">
+              {getAvailableTimeOptions(selectedDate)}
+            </select>
+          </div>
+          <div className="flex flex-col mx-1">
+            <label htmlFor="user" className="text-sm text-violet-600">
+              To
+            </label>
+            <select {...register("endHour")} className="border rounded p-1">
+              {getAvailableTimeOptions(selectedDate)}
+            </select>
+          </div>
+          <div className="m-1 p-1 flex items-end gap-1">
             <input
               {...register("endNextDay")}
               id="endNextDay"
               type="checkbox"
+              className="h-5"
             />
             <label htmlFor="endNextDay"> Until next day</label>
           </div>
-          <div>
+          <div className="ml-2 flex items-end">
             <button
               type="submit"
               className="m-1 p-1 w-20 rounded border bg-violet-100 border-violet-400 text-violet-900"
@@ -106,7 +157,9 @@ const AddReservation = ({ selectedDate }: { selectedDate: Date }) => {
           <span className="text-red-400 text-sm">{errors.user.message}</span>
         )}
         {errors.startHour && (
-          <span className="text-red-400 text-sm">{errors.startHour.message}</span>
+          <span className="text-red-400 text-sm">
+            {errors.startHour.message}
+          </span>
         )}
         {errors.endHour && (
           <span className="text-red-400 text-sm">{errors.endHour.message}</span>
---
+++ hooks/useReservationValidator.tsx
@@ -0,0 +1,69 @@
+import { compareAsc, format, getOverlappingDaysInIntervals } from "date-fns";
+import React from "react";
+import { Reservation } from "../lib/useReservation";
+
+const useReservationValidator = (reservations: Reservation[]) => {
+  const isUserUniqueInDay = (user: string, selectedDate: Date) => {
+    return (
+      reservations.find(
+        (reserve) =>
+          format(reserve.startDate, "yyyyMMdd") ===
+            format(selectedDate, "yyyMMdd") && reserve.user === user
+      ) === undefined
+    );
+  };
+
+  const isEndHourValid = (
+    endNextDay: boolean,
+    endHour: number,
+    startHour: number
+  ) => endNextDay || endHour - startHour >= 1;
+
+  const isEndHourInValidOnNextDay = (endHour: number) => endHour <= 11;
+
+  const hasSelectedRangeOverlap = (startDate: Date, endDate: Date): boolean => {
+    const endNextDay:boolean=format(startDate, "yyyyMMdd")!==format(endDate, "yyyMMdd");
+
+    const filteredReservations = reservations.filter((reserve: Reservation) =>
+      [
+        format(reserve.startDate, "yyyyMMdd"),
+        format(reserve.endDate, "yyyMMdd"),
+      ].includes(format(startDate, "yyyMMdd"))||[
+        format(reserve.startDate, "yyyyMMdd"),
+        format(reserve.endDate, "yyyMMdd"),
+      ].includes(format(endDate, "yyyMMdd"))
+    );
+    const sortedReservations = filteredReservations.sort(
+      (a: Reservation, b: Reservation) => compareAsc(a.startDate, b.startDate)
+    );
+    const startHour: number = startDate.getHours();
+    const endHour: number = endDate.getHours();
+
+    let overlap = sortedReservations.find((reserve) => {
+      const reserveStartDate: Date = reserve.startDate;
+      const reserveEndDate: Date = reserve.endDate;
+
+      return (
+        reserveStartDate === startDate ||
+        reserveEndDate === endDate ||
+        (reserveStartDate < startDate && reserveEndDate > startDate) ||
+        (endNextDay&& reserveStartDate < endDate && reserveEndDate > endDate)||
+        (startDate < reserveStartDate &&  endDate>reserveStartDate) ||
+        (startDate < reserveEndDate &&  endDate>reserveEndDate) 
+        
+      );
+    });
+console.log(overlap);
+
+    return overlap != undefined;
+  };
+
+  return {
+    isUserUniqueInDay,
+    isEndHourValid,
+    isEndHourInValidOnNextDay,
+    hasSelectedRangeOverlap,
+  };
+};
+
+export default useReservationValidator;
